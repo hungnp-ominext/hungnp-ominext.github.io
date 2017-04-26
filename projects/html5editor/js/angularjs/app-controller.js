508294689fb365a06e515786be1737eca70e1e0b
hungnp <hungnp@ominext.com>
2017-04-26 18:13:14 +0700

    fix create text

--- projects/html5editor/js/angularjs/app-controller.js
+++ projects/html5editor/js/angularjs/app-controller.js
@@ -9,6 +9,7 @@ function AppController($scope) {
         width: DEFAULT_WIDTH,
         height: DEFAULT_HEIGHT
     };
+    $scope.canvasObjects = [];
     $scope.canvasPoster = null;
     // $scope.getActiveStyle = getActiveStyle;
     $scope.decreaseZoom = function () {
@@ -48,12 +49,36 @@ function AppController($scope) {
             hoverCursor: "default",
             alwaysBottom: true,
             posterType: "poster",
-            evented: false
+            evented: false,
+            name: 'Poster'
         });
         canvas.add($scope.canvasPoster);
-        $scope.canvasPoster.sendBackwards();
+        $scope.canvasPoster.moveTo(0);
         $scope.canvasPoster.center();
         $scope.canvasPoster.setCoords();
+        $scope.getObjects();
+    };
+    $scope.createText = function (text, fontSize, bold, italic) {
+        var textProperties = {
+            fontSize: fontSize,
+            width: 300,
+            shadow: {
+                color: 'rgba(0,0,0,1)',
+                blur: 0,
+                offsetX: 0,
+                offsetY: 0,
+                position: 7
+            },
+            editable: true,
+            name: 'Text'
+        };
+        if (bold) textProperties.fontWeight = 'bold';
+        if (italic) textProperties.fontStyle = 'italic';
+        var txt = new fabric.Textbox(text, textProperties);
+        canvas.add(txt);
+        txt.center();
+        txt.setCoords();
+        $scope.getObjects();
     };
     $scope.tabLeftActive = {
         tabCreateActive: true,
@@ -61,6 +86,35 @@ function AppController($scope) {
         tabTextActive: false,
         tabUploadActive: false,
         tabTemplateActive: false
+    };
+    $scope.uploadFileDragend = function (ev) {
+        return false;
+    };
+    $scope.uploadFileDragover = function (ev) {
+        return false;
+    };
+    $scope.uploadFileDrop = function (ev, item) {
+        ev.preventDefault();
+
+        var file = ev.dataTransfer.files[0],
+            reader = new FileReader();
+        reader.onload = function (event) {
+            console.log(event.target);
+        };
+        console.log(file);
+        reader.readAsDataURL(file);
+        return false;
+    };
+    $scope.getObjects = function () {
+        $scope.canvasObjects = [];
+        var objects = canvas.getObjects();
+        angular.forEach(objects, function (value, key) {
+            var obj = {};
+            obj.display = value.opacity !== 0;
+            obj.name = value.name + ' (' + value.width.toFixed(0) + '*' + value.height.toFixed(0) + ')';
+            obj.image = value.toDataURL();
+            $scope.canvasObjects.push(obj);
+        })
     };
 
     $scope.activeTab = function (tab) {
